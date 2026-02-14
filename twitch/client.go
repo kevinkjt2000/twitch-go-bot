f600e7df87b61ade360268460f095d8c262c38bf
Kevin Tindall <kevinkjt2000@gmail.com>
2026-02-14 10:04:37 +0100

    Mention usernm0 circuits

--- twitch/client.go
+++ twitch/client.go
@@ -142,7 +142,7 @@ func NewClient(ctx context.Context, conf Config) (Client, error) {
 		} else if bytes.Equal(squashedMsgline, []byte("!modpack")) {
 			ircClient.Say(string(channel), "Check out GTNH's wiki here https://wiki.gtnewhorizons.com/wiki/Main_Page", false)
 		} else if bytes.Equal(squashedMsgline, []byte("!textures")) {
-			ircClient.Say(string(channel), "Using F32 packs and outlined ores from https://gtnh.miraheze.org/wiki/Resource_Packs", false)
+			ircClient.Say(string(channel), "Using F32 packs, outlined ores, and Usernm0 circuits from https://gtnh.miraheze.org/wiki/Resource_Packs", false)
 		} else if bytes.Equal(squashedMsgline, []byte("!youtube")) {
 			ircClient.Say(string(channel), "http://www.youtube.com/@shinybucket", false)
 		}
